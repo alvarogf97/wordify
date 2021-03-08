cfa8d9d42fbf1caab4b06feaf14277e3d557b079
lauragogarrido <laura-gomez@hotmail.es>
2021-03-08 10:42:42 +0100

    endpoint phrases random and test. I have a module not found error with flask when running test but flask is install with poetry

--- wordify/handlers/users.py
+++ wordify/handlers/users.py
@@ -1,4 +1,16 @@
 from flask import Blueprint
+import random
+from services.user_service.user_service_json import retrieve_by_id
+from serializers import user_serializer
 
 
 user_bp = Blueprint('user', __name__)
+
+
+@user_bp.route('/users/<int:id>/phrases/random')
+def phrases_random(id):
+        """ return a random phrases of a given user **id**"""
+        user = retrieve_by_id(id)
+        user_phrases = user_serializer(user)['phrases']
+        phrase = random.randrange(len(user_phrases))
+        return phrase
